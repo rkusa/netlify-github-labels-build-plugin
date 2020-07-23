9148c6ed0b9c118d70bbc0a268f7013d2622d508
Markus Ast <m@rkusa.st>
2020-07-23 17:28:43 +0200

    call github api

--- github-labels-filter/src/index.js
+++ github-labels-filter/src/index.js
@@ -6,9 +6,62 @@ const octokit = new Octokit({
 
 module.exports = {
   async onPreBuild({constants, utils}) {
+    const labelName = getLabelName()
+    if (!ownerRepo) {
+      console.warn("Failed to label name")
+      return
+    }
+
+    const ownerRepo = getOwnerRepo()
+    if (!ownerRepo) {
+      console.warn("Failed to extract owner and repo")
+      return
+    }
+
+    const prNumber = getPrNumber()
+    if (!prNumber) {
+      console.warn("Failed to extract PR number")
+      return
+    }
+
+    const { data: labels } = await octokit.issues.listLabelsOnIssue({
+      ...ownerRepo,
+      issue_number: prNumber,
+      per_page: 100
+    })
+
+
     console.log(constants)
     console.log(utils.git)
     console.log(process.env)
+    console.log(labels)
     console.log('Hello world from onPreBuild event!')
   },
+}
+
+function getOwnerRepo() {
+  const match = process.env.REPOSITORY_URL.match(/https:\/\/github\.com\/([^\/]+)\/([^\/]+)/)
+  if (!match) {
+    return null
+  }
+
+  return {owner: match[1], repo: match[2]}
+}
+
+function getPrNumber() {
+  const match = process.env.BRANCH.match(/pull\/(\d+)\/head/)
+  if (!match) {
+    return null
+  }
+
+  return match[1]
+}
+
+function getLabelName() {
+  switch (process.env.SITE_NAME) {
+    case 'nostalgic-varahamihira-3bbbd3':
+      return 'deploy-nostalgic-varahamihira'
+    default:
+      return null
+  }
 }

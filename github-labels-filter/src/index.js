292f8d01763ccc086090958249ecc98a65aaebd1
Markus Ast <m@rkusa.st>
2020-07-23 17:06:57 +0200

    init sdk

--- github-labels-filter/src/index.js
+++ github-labels-filter/src/index.js
@@ -1,3 +1,9 @@
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+
 module.exports = {
   async onPreBuild({constants, utils}) {
     console.log(constants)
